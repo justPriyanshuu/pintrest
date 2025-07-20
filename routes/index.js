c6fc7c98e69f0d511bec1beade79b9d007f5b544
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-07-20 13:24:23 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,60 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
+const passport = require("passport");
+const localStrategy = require("passport-local");
 
+passport.use(new localStrategy(userModel.authenticate()));
 
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get("/", function (req, res, next) {
+  res.render("index");
 });
 
-router.get('/register', function(req, res, next) {
-  res.render('register');
+router.get("/register", function (req, res, next) {
+  res.render("register");
 });
+
+router.get("/profile", isLoggedIn, function (req, res, next) {
+  res.render("profile");
+});
+
+router.post("/register", function (req, res, next) {
+  const data = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    contact: req.body.contact,
+  });
+
+  userModel.register(data, req.body.password).then(() => {
+    passport.authenticate("local")(req, res, () => {
+      res.redirect("/profile");
+    });
+  });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/",
+    successRedirect: "/profile",
+  }),
+  function (req, res, next) {}
+);
+
+router.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
 
 module.exports = router;
