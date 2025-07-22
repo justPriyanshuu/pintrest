fd4eb7eb0590d0aa39bbd1e568b84ec5a793c434
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-07-22 22:04:30 +0530

    update

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
-const plm = require("passport-local-mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/pintrest");
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/pintrest');
 
 const userSchema = mongoose.Schema({
   username: String,
@@ -13,8 +13,14 @@ const userSchema = mongoose.Schema({
     type: Array,
     default: [],
   },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'post',
+    },
+  ],
 });
 
 userSchema.plugin(plm);
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model('user', userSchema);
